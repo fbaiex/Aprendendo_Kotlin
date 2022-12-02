a0f43d5d7b81862ad31a368c70968c1bd2be226f
Fabio Aiex Alves <fabio.aiex@hotmail.com>
2022-12-02 10:56:14 -0300

    AprendendoKotlin.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,56 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+fun main() {
+    println("Bem vindo ao Byte Bank")
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    var i = 0
+
+    while(i < 5){
+        val titular = "Alexa $i"
+        val numeroConta = 1000 + i
+        var saldoConta = i + 10.0
+
+
+        println("Titular: $titular")
+        println("Número da conta $numeroConta")
+        println("Saldo da conta $saldoConta")
+        println(i)
+        println()
+        i++
+    }
+
+//    for (i in 5 downTo 1) {
+//
+//        val titular = "Alexa $i"
+//        val numeroConta = 1000 + i
+//        var saldoConta = i + 10.0
+//
+//
+//        println("Titular: $titular")
+//        println("Número da conta $numeroConta")
+//        println("Saldo da conta $saldoConta")
+//        println(i)
+//        println()
+//    }
+
+//    testaCondicoes(saldoConta)
+
+}
+
+fun testaCondicoes(saldoConta: Double) {
+
+    if (saldoConta > 0.0) {
+        println("Conta está com saldo positivo")
+    } else if (saldoConta == 0.0) {
+        println("Sua conta está zerada")
+    } else {
+        println("Conta é negativa")
+    }
+
+    when {
+        saldoConta > 0.0 -> println("Conta está com saldo positivo")
+
+        saldoConta == 0.0 -> println("Sua conta está zerada")
+
+        else -> println("Conta é negativa")
+
+    }
 }
